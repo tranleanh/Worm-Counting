f24632bdd87e7f21bf04d6d8b8cfb65464e38cff
Tran Le Anh <tranleanh.nt@gmail.com>
2020-04-01 19:44:44 +0900

    Update worm_counting.py

--- worm_counting.py
+++ worm_counting.py
@@ -8,21 +8,19 @@ import matplotlib.pyplot as plt
 
 def count_contours(img):
 
-    cnts, h = cv2.findContours(img, 1, 2)
-    cnts = np.asarray(cnts)
-    
-    return cnts.shape[0]
+	cnts, h = cv2.findContours(img, 1, 2)
+	cnts = np.asarray(cnts)
+	
+	return cnts.shape[0]
 
 def closing_process(img, kernel_size = 5):
-    
-    c_kernel = np.ones((kernel_size, kernel_size), np.uint8)
-    closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, c_kernel)
-    
-    return closing
+	
+	c_kernel = np.ones((kernel_size, kernel_size), np.uint8)
+	closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, c_kernel)
+	
+	return closing
 
 def main():
-
-	img_name = 'worm.jpg'
 
 	img = cv2.imread(img_name, 0)
 	edges = cv2.Canny(img, 50, 100)
@@ -31,15 +29,26 @@ def main():
 
 	processed_img = closing_process(thresh1)
 
-	print('Number of Worms: ', count_contours(processed_img))
+	final_count = count_contours(processed_img) - 2
+
+	print('Image: ', img_name)
+	print('Number of Worms: ', final_count)
 
 	plt.subplot(121),plt.imshow(img, cmap = 'gray')
-	plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+	plt.title('Image: ' + img_name), plt.xticks([]), plt.yticks([])
 	plt.subplot(122),plt.imshow(thresh1, cmap = 'gray')
-	plt.title('Number of Worms: ' + str(count_contours(processed_img))), plt.xticks([]), plt.yticks([])
+	plt.title('Number of Worms: ' + str(final_count)), plt.xticks([]), plt.yticks([])
 	
 	plt.show()
 
 if __name__ == '__main__':
 	
+# ===================================== #
+	# CHANGE IMAGE FILE NAME HERE! #
+
+	# img_name = 'worm_4_cropped.jpg'
+	img_name = '78.jpg'
+
+# ===================================== #
+
 	main()
